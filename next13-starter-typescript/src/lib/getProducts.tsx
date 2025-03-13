d539ee97534fceea7bf1d6c0c5b4b5a636014b78
trantuananh16032002 <trantuananh16032002@gmail.com>
2025-03-13 11:38:51 +0700

    3/13/4

--- next13-starter-typescript/src/lib/getProducts.tsx
+++ next13-starter-typescript/src/lib/getProducts.tsx
@@ -1,11 +1,11 @@
 export const getProducts = async () => {
   try {
-    const res = await fetch("http://localhost:5000/api/products", { cache: "no-store" });
+    const res = await fetch("http://45.32.116.67:5000/api/products", { cache: "no-store" });
     if (!res.ok) {
       throw new Error("Failed to fetch");
     }
     const products = await res.json();
-    return products.slice(0, 4); // Chỉ lấy 3 sản phẩm đầu tiên
+    return products.slice(0, 4); 
   } catch (error) {
     console.error(error);
     return [];
